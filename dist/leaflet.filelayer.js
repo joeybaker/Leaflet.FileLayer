984fae82e938d8cf8ff5cf801e9df856c91b9ede
Joey Baker <joey.baker@ubnt.com>
2013-11-12 12:49:49 -0800

    Prefix all the commas #cleanup

--- dist/leaflet.filelayer.js
+++ dist/leaflet.filelayer.js
@@ -21,175 +21,190 @@ var L = window.L
 , FileLoader = L.Class.extend({
   includes: L.Mixin.Events,
   options: {
-    layerOptions: {},
-    addToMap: true
-  },
+    layerOptions: {}
+    , addToMap: true
+  }
 
-  initialize: function(map, options){
-    this._map = map;
-    L.Util.setOptions(this, options);
+  , initialize: function(map, options){
+    this._map = map
+    L.Util.setOptions(this, options)
 
     this._parsers = {
       'geojson': this._loadGeoJSON,
       'gpx': this._convertToGeoJSON,
       'kml': this._convertToGeoJSON
-    };
-  },
+    }
+  }
 
-  load: function(file){
+  , load: function(file){
     // Check file extension
-    var ext = file.name.split('.').pop(),
-      parser = this._parsers[ext];
+    var ext = file.name.split('.').pop()
+      , parser = this._parsers[ext]
+      // Read selected file using HTML5 File API
+      , reader = new FileReader()
+
     if (!parser){
-      window.alert('Unsupported file type ' + file.type + '(' + ext + ')');
-      return;
+      window.alert('Unsupported file type ' + file.type + '(' + ext + ')')
+      return
     }
-    // Read selected file using HTML5 File API
-    var reader = new FileReader();
+
     reader.onload = L.Util.bind(function(e){
+      var layer
+
       this.fire('data:loading', {
-        filename: file.name,
-        format: ext
-      });
-      var layer = parser.call(this, e.target.result, ext);
+        filename: file.name
+        , format: ext
+      })
+
+      layer = parser.call(this, e.target.result, ext)
+
       this.fire('data:loaded', {
-        layer: layer,
-        filename: file.name,
-        format: ext
-      });
-    }, this);
-    reader.readAsText(file);
-  },
+        layer: layer
+        , filename: file.name
+        , format: ext
+      })
+    }, this)
+    reader.readAsText(file)
+  }
 
-  _loadGeoJSON: function(content){
-    var geojson = L.geoJson(content, this.options.layerOptions);
+  , _loadGeoJSON: function(content){
+    var geojson = L.geoJson(content, this.options.layerOptions)
+
     if (typeof content === 'string'){
-      content = JSON.parse(content);
+      content = JSON.parse(content)
     }
-    return this.options.addToMap ? geojson.addTo(this._map) : geojson;
-  },
+    return this.options.addToMap ? geojson.addTo(this._map) : geojson
+  }
 
-  _convertToGeoJSON: function(content, format){
+  , _convertToGeoJSON: function(content, format){
     // Format is either 'gpx' or 'kml'
     if (typeof content === 'string'){
-      content = (new window.DOMParser()).parseFromString(content, 'text/xml');
+      content = (new window.DOMParser()).parseFromString(content, 'text/xml')
     }
-    var geojson = togeojson[format](content);
-    return this._loadGeoJSON(geojson);
+
+    return this._loadGeoJSON(togeojson[format](content))
   }
-});
+})
 
 L.Control.FileLayerLoad = L.Control.extend({
   statics: {
-    TITLE: 'Load local file (GPX, KML, GeoJSON)',
-    LABEL: '&#8965;'
+    title: 'Load local file (GPX, KML, GeoJSON)'
+    , label: 'Upload'
+    , className: 'leaflet-control-filelayer'
+    , barName: 'leaflet-bar'
+    , barPartName: 'leaflet-bar-part'
   },
   options: {
-    position: 'topleft',
-    fitBounds: true,
-    layerOptions: {}
-  },
+    position: 'topleft'
+    , fitBounds: true
+    , layerOptions: {}
+  }
 
-  initialize: function(options){
-    L.Util.setOptions(this, options);
-    this.loader = null;
-  },
+  , initialize: function(options){
+    L.Util.setOptions(this, options)
+    this.loader = null
+  }
 
-  onAdd: function(map){
-    this.loader = new FileLoader(map, this.options);
+  , onAdd: function(map){
+    this.loader = new FileLoader(map, this.options)
 
     this.loader.on('data:loaded', function(e){
       // Fit bounds after loading
       if (this.options.fitBounds){
         window.setTimeout(function(){
-          map.fitBounds(e.layer.getBounds()).zoomOut();
-        }, 500);
+          map.fitBounds(e.layer.getBounds())
+        }, 200)
       }
-    }, this);
+    }, this)
 
     // Initialize Drag-and-drop
-    this._initDragAndDrop(map);
+    this._initDragAndDrop(map)
 
     // Initialize map control
-    return this._initContainer();
-  },
+    return this._initContainer()
+  }
 
-  _initDragAndDrop: function(map){
+  , _initDragAndDrop: function(map){
     var fileLoader = this.loader
       , dropbox = map._container
       , callbacks = {
         dragenter: function(){
-          map.scrollWheelZoom.disable();
+          map.scrollWheelZoom.disable()
         },
         dragleave: function(){
-          map.scrollWheelZoom.enable();
+          map.scrollWheelZoom.enable()
         },
         dragover: function(e){
-          e.stopPropagation();
-          e.preventDefault();
+          e.stopPropagation()
+          e.preventDefault()
         },
         drop: function(e){
           var files = Array.prototype.slice.apply(e.dataTransfer.files)
             , loadInterval = 25
 
           ;(function dropLoad(){
-            fileLoader.load(files.shift());
+            fileLoader.load(files.shift())
             if (files.length > 0){
               setTimeout(dropLoad, loadInterval)
             }
           })()
 
-          e.stopPropagation();
-          e.preventDefault();
+          e.stopPropagation()
+          e.preventDefault()
 
-          map.scrollWheelZoom.enable();
+          map.scrollWheelZoom.enable()
         }
-      };
+      }
 
     for (var name in callbacks)
-      dropbox.addEventListener(name, callbacks[name], false);
-  },
+      dropbox.addEventListener(name, callbacks[name], false)
+  }
 
-  _initContainer: function(){
+  , _initContainer: function(){
     // Create an invisible file input
     var fileLoader = this.loader
     // Create a button, and bind click on hidden file input
-      , zoomName = 'leaflet-control-filelayer leaflet-control-zoom'
-      , barName = 'leaflet-bar'
-      , partName = barName + '-part'
-      , container = L.DomUtil.create('div', zoomName + ' ' + barName)
-      , fileInput = L.DomUtil.create('input', 'hidden', container)
-      , link = L.DomUtil.create('a', zoomName + '-in ' + partName, container)
-      , stop = L.DomEvent.stopPropagation;
+      , baseClassName = L.Control.FileLayerLoad.className
+      , barClassName = L.Control.FileLayerLoad.barName
+      , partName = L.Control.FileLayerLoad.barPartName
+      , container = L.DomUtil.create('div', baseClassName)
+      , bar = L.DomUtil.create('div', barClassName, container)
+      , link = L.DomUtil.create('a', baseClassName + ' ' + partName, bar)
+      , fileInput = L.DomUtil.create('input', 'hidden', link)
+      , onclick
 
-    fileInput.type = 'file';
-    fileInput.accept = '.gpx,.kml,.geojson';
-    fileInput.style.display = 'none';
+    fileInput.type = 'file'
+    fileInput.accept = '.gpx,.kml,.geojson'
+    fileInput.style.display = 'none'
     // Load on file change
     fileInput.addEventListener('change', function(){
-      fileLoader.load(this.files[0]);
-    }, false);
+      fileLoader.load(this.files[0])
+      // reset so that the user can upload the same file again if they want to
+      this.value = ''
+    }, false)
 
-    link.innerHTML = L.Control.FileLayerLoad.LABEL;
-    link.href = '#';
-    link.title = L.Control.FileLayerLoad.TITLE;
+    link.innerHTML = L.Control.FileLayerLoad.label
+    link.href = '#'
+    link.title = L.Control.FileLayerLoad.title
 
-    L.DomEvent
-      .on(link, 'click', stop)
-      .on(link, 'mousedown', stop)
-      .on(link, 'dblclick', stop)
-      .on(link, 'click', L.DomEvent.preventDefault)
-      .on(link, 'click', function(e){
-        fileInput.click();
-        e.preventDefault();
-      });
-    return container;
+    L.DomEvent.disableClickPropagation(link)
+    onclick = function(){
+      fileInput.click()
+    }
+
+    L.DomEvent.on(link, 'click', onclick)
+
+    // expose dom so the user can mess with it
+    this.button = link
+    this.fileInput = fileInput
+    this.onclick = onclick
+    return container
   }
-});
+})
 
 L.Control.fileLayerLoad = function(options){
-  return new L.Control.FileLayerLoad(options);
-};
+  return new L.Control.FileLayerLoad(options)
+}
 
     return FileLoader;
 }));
